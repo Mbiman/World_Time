44ed91407a20d1bf8cb2eb36e7b9d18601445af0
mbiman <juniormbi80@gmail.com>
2022-09-09 12:51:38 +0100

    flutter http package

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,27 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    //simulate a network request for username
-    String username = await Future.delayed(const Duration(seconds: 3), () {
-      return 'mbiman';
-    });
-
-    //simulate a network request to get bio of the user
-    String bio = await Future.delayed(const Duration(seconds: 2), () {
-      return 'Bio of mbiman';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('Hey there!!');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
