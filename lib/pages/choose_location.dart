7f59190674caea3c696dbc353c0299cc4652efc9
mbiman <juniormbi80@gmail.com>
2022-09-10 10:49:05 +0100

    flag images

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key? key}) : super(key: key);
@@ -8,6 +9,28 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(location: 'London', flag: 'uk.png', endPoint: 'Europe/London'),
+    WorldTime(
+        location: 'Berlin', flag: 'greece.png', endPoint: 'Europe/berlin'),
+    WorldTime(location: 'Cairo', flag: 'egypt.png', endPoint: 'Africa/Cairo'),
+    WorldTime(
+        location: 'Nairobi', flag: 'kenya.png', endPoint: 'Africa/Nairobi'),
+    WorldTime(
+        location: 'Chicago', flag: 'usa.png', endPoint: 'America/Chicago'),
+    WorldTime(
+        location: 'New_York', flag: 'usa.png', endPoint: 'America/New_york'),
+    WorldTime(
+        location: 'Seoul', flag: 'south_korea.png', endPoint: 'Asia/Seoul'),
+    WorldTime(
+        location: 'Jarkata', flag: 'indonesia.png', endPoint: 'Asia/Jakarta'),
+    WorldTime(location: 'Lagos', flag: 'ngn.png', endPoint: 'Afica/Lagos'),
+    WorldTime(
+        location: 'Johannesburg',
+        flag: 'za.png',
+        endPoint: 'Afica/Johannesburg'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +40,20 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: const Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              onTap: () {},
+              title: Text(locations[index].location),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/${locations[index].flag}'),
+              ),
+            ),
+          );
+        },
+        itemCount: locations.length,
       ),
     );
   }
